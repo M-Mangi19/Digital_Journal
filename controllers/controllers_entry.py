46c21f9e6532d147a7b2e63077b4753cc202be30
Matthew Mangiaracina <mmangiaracina99@gmail.com>
2023-07-17 18:36:23 -0400

    delete files

--- controllers/controllers_entry.py
+++
@@ -1,8 +0,0 @@
-from flask_app import app
-from flask import Flask, request, render_template, redirect, session, flash
-from flask_app.models.models_user import User
-from flask_app.models.models_entry import Entry
-from flask_app.models.models_favorite import Favorite
-from flask_bcrypt import Bcrypt
-
-bcrypt = Bcrypt(app)
--- flask_app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-app.secret_key = "skipping on the beat like ali at the gym"
