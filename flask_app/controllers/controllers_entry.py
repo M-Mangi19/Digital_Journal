6091e1f9590bc454cc27d1df41faa2264068c6bd
Matthew Mangiaracina <mmangiaracina99@gmail.com>
2023-07-22 15:18:05 -0400

    added edit page

--- flask_app/controllers/controllers_entry.py
+++ flask_app/controllers/controllers_entry.py
@@ -6,3 +6,91 @@ from flask_app.models.models_favorite import Favorite
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt(app)
+
+#Don't forget to add in protected routes (must be logged in to use)
+
+#Homepage
+@app.route('/homepage')
+def homepage():
+    if 'user_id' not in session:
+        return redirect('/')
+    else:
+        data = {
+            'id' : session['user_id']
+        }
+        user = User.get_one(data)
+        return render_template('homepage.html', user = user)
+
+#Create Entry Page
+@app.route('/create/entry')
+def create_entry():
+    if 'user_id' not in session:
+        return redirect('/')
+    else:
+        user_data = {
+            'id' : session['user_id']
+        }
+        user = User.get_one(user_data)
+        return render_template('journal_entry.html', user = user)
+
+#Create Entry
+@app.route('/create/new_entry', methods=['POST'])
+def create_new_entry():
+    if not Entry.entry_validator(request.form):
+        return redirect('/create/entry')
+    data = {
+        'content' : request.form['content'],
+        'date' : request.form['date'],
+        'user_id' : session['user_id']
+    }
+    Entry.create(data)
+    return redirect('/create/entry')
+
+#Edit page
+@app.route('/edit/<int:entry_id>')
+def edit(entry_id):
+    if 'user_id' not in session:
+        return redirect('/')
+    data = {
+        'id' : entry_id
+    }
+    entry = Entry.get_one(data)
+    return render_template('edit_entry.html', entry = entry)
+
+#Update
+@app.route('/update/entry/<int:entry_id>', methods=['POST'])
+def update(entry_id):
+    if 'user_id' not in session:
+        return redirect('/')
+    if not Entry.entry_validator(request.form):
+        return redirect('/create/entry')
+        # could not get it to redirect back to the edit page.
+        # Wasn't sure how to grab the <int:show_id> in the redirect
+        # so i just sent it to the create/show page displaying the validator it was
+        # not fulfilling with in the edit page
+    Entry.update(request.form, entry_id)
+    return redirect('/create/entry')
+
+#View
+@app.route('/view/entry/<int:entry_id>')
+def view(entry_id):
+    if 'user_id' not in session:
+        return redirect('/')
+    data = {
+        'id' : session['user_id']
+    }
+    entry_data = {
+        'id' : entry_id
+    }
+    user = User.get_one(data)
+    # entries = Entry.get_user_with_entry(entry_data)
+    return render_template('view.html', user = user)
+
+
+#Delete
+@app.route('/delete/<int:entry_id>')
+def delete(entry_id):
+    if 'user_id' not in session:
+        return redirect('/')
+    Entry.delete(entry_id)
+    return redirect('/create/entry')
--- flask_app/models/models_entry.py
+++ flask_app/models/models_entry.py
@@ -10,6 +10,68 @@ class Entry:
 
     def __init__(self,data):
         self.id = data['id']
-        self.content = content['content']
+        self.content = data['content']
+        self.date = data['date']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.user = data['user']
+
+#Create
+    @classmethod
+    def create(cls, data):
+        query = """
+                INSERT INTO entries (content, date, user_id)
+                VALUE (%(content)s, %(date)s, %(user_id)s);
+                """
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+#Get All
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM entries;"
+        results = connectToMySQL(db).query_db(query)
+        entries = []
+        for entry in results:
+            entries.append(cls(entry))
+        return entries
+
+#Get One
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM entries WHERE id = %(id)s"
+        results = connectToMySQL(db).query_db(query, data)
+        print(results)
+        return cls(results[0])
+
+
+
+#Update
+    @classmethod
+    def update(cls, form_data, entry_id):
+        query = f"UPDATE entries SET content=%(content)s, date=%(date)s WHERE id = {show_id}"
+        return connectToMySQL(db).query_db(query, form_data)
+#Delete
+    @classmethod
+    def delete(cls, entry_id):
+        query = "DELETE from entries WHERE id = %(id)s"
+        data ={'id': entry_id}
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+
+#One to Many
+
+#One to Many
+
+#Create and Edit Validators
+    @staticmethod
+    def entry_validator(data):
+            is_valid = True
+            if len(data['content']) <= 0:
+                flash("Must have content")
+                is_valid = False
+            if len(data['date']) <= 0:
+                flash("Date is required")
+                is_valid = False
+            return is_valid
